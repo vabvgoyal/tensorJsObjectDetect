f36589439e06bdd6168807c90208ba8cc5289ff3
Vaibhav Goyal <vaibhav.goyal@external.swiggy.in>
2021-06-07 12:21:43 +0530

    build

---
+++ build/precache-manifest.162131bb4cc736ec28cdc01e6eaa802c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51ec5b584cb64f94eacb2bdf88318ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0839d78a17f798b1215",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "14f24aec00ff850822cf",
+    "url": "/static/js/2.598297bb.chunk.js"
+  },
+  {
+    "revision": "e8360af5817953cec407b3cdee6b2ff8",
+    "url": "/static/js/2.598297bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0839d78a17f798b1215",
+    "url": "/static/js/main.1de8d97a.chunk.js"
+  },
+  {
+    "revision": "0ae5a039c8f9cb9fadc9",
+    "url": "/static/js/runtime-main.6d7e2fe7.js"
+  }
+]);
