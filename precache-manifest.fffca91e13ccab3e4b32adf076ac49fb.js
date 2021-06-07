317b89d182cd159d0a45d684feb7797d33e20e11
Vaibhav Goyal <vaibhav.goyal@external.swiggy.in>
2021-06-07 12:44:08 +0530

    Updates

---
+++ precache-manifest.fffca91e13ccab3e4b32adf076ac49fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd78c27956f7ffd52efdf5035bb8ed3d",
+    "url": "/tensorJsObjectDetect/index.html"
+  },
+  {
+    "revision": "e0839d78a17f798b1215",
+    "url": "/tensorJsObjectDetect/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "14f24aec00ff850822cf",
+    "url": "/tensorJsObjectDetect/static/js/2.598297bb.chunk.js"
+  },
+  {
+    "revision": "e8360af5817953cec407b3cdee6b2ff8",
+    "url": "/tensorJsObjectDetect/static/js/2.598297bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0839d78a17f798b1215",
+    "url": "/tensorJsObjectDetect/static/js/main.1de8d97a.chunk.js"
+  },
+  {
+    "revision": "2c1af008df23b69ff6dc",
+    "url": "/tensorJsObjectDetect/static/js/runtime-main.01a137bd.js"
+  }
+]);
